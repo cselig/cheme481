43a00ac931473f53b15b1d15feebb797145bf4b9
cselig <cselig@uw.edu>
2016-11-29 08:48:13 -0800

    Update Smin.m

--- Smin.m
+++ Smin.m
@@ -1,17 +1,25 @@
 function [xmin fmin iter] = Smin(fn,xstart,errx,errf)
 
+N = length(xstart);
 f = str2func(fn);
-N = length(xstart)
+xcurr = xstart;
+xprev = zeros(1, N);
+fcurr = f(xstart);
+fprev = 0;
+
 % loop
+while abs(xcurr - xprev) > errx && abs(fprev - fcurr) > errf
+    fprev = fcurr;
     % select direction to search
-    % s = - gradient
+    grad = findGradient(fn, xstart, N);
+    s = -1 .* grad;
+    s = s ./ norm(s);
+    [lamda, fcurr] = lineSearch(fn, xcurr, s);
+    xprev = xcurr;
+    xcurr = xcurr + lamda .* s;
     % line search
-    
-x = 5;
-y = 5;
-    
-xrange = [x-1:0.1:x+1];
-yrange = [y-1:0.1:y+1];
+
+
 xmin = 0;
 fmin = 0;
 iter = 0;
