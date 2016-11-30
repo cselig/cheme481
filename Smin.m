85c5a65dc8d5ff08c750fefe52f368c2dbe6b566
cselig <cselig@uw.edu>
2016-11-29 16:54:59 -0800

    Update Smin.m

--- Smin.m
+++ Smin.m
@@ -1,29 +1,39 @@
-function [xmin fmin iter] = Smin(fn,xstart,errx,errf)
+function [xmin, fmin, iter] = Smin(fn,xstart,errx,errf)
 
+% Initialize values. N is dimension of problem space
 N = length(xstart);
 f = str2func(fn);
 xcurr = xstart;
 xprev = zeros(1, N);
 fcurr = f(xstart);
 fprev = 0;
+iter = 0;
 i = 0;
 maxItr = 10000;
+first = true;
+repeat = true;
 
-while norm(xcurr - xprev) > errx && abs(fprev - fcurr) > errf
+% Keep searching in direction determined by gradient until stopping 
+% criteria met. Will always enter loop first time
+while repeat || first
+    first = false;
+    fprev = fcurr;
     % select direction to search
-    grad = findGradient(fn, xstart, N);
-    s = -1 .* grad;
-    s = s ./ norm(s);
-    [lamda, fcurr] = lineSearch(fn, xcurr, s);
+    [s, gradItr] = findGradient(fn, xcurr, N);
+    iter = iter + gradItr;
+    [lamda, searchItr] = lineSearch(fn, xcurr, s);
+    iter = iter + searchItr;
     xprev = xcurr;
     xcurr = xcurr + lamda .* s;
-    fprev = fcurr;
     fcurr = f(xcurr);
     i = i + 1;
     if i > maxItr
-       error('Max number of functional evaluations exceeded') 
+       error('Max number of function evaluations exceeded') 
     end
+    % Check stopping criteria
+    repeat = determineRepeat(xcurr, xprev, fprev, fcurr, errx, errf);
 end
+% Return results
 xmin = xcurr;
 fmin = fprev;
-iter = i;
+iter = iter + i;
