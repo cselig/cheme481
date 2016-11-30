0cd9e052b6da89c5810085b95104d73165c095d6
cselig <cselig@uw.edu>
2016-11-29 16:56:10 -0800

    Update findGradient.m

--- findGradient.m
+++ findGradient.m
@@ -1,10 +1,11 @@
-function grad = findGradient(fn, x0, N)
+function [s, itr] = findGradient(fn, x0, N)
 % determine gradient of f at x0
 
-% Initialize values. N is dimension of problem space
+% Initialize values
 f = str2func(fn);
 grad = zeros(1, N);
 h = 0.00001;
+itr = 0;
 
 % Determine gradient using finite forward difference
 for i = 1:N
@@ -13,7 +14,9 @@ for i = 1:N
    x1(i) = x1(i) + h;
    f1 = f(x1);
    grad(i) = (f1 - f0) / h;
+   itr = itr + 2;
 end
-
+% Convert gradient into direction along which to search
+s = -1 .* grad;
+s = s ./ norm(s);
 end
-
