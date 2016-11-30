5e95339dc55c081f0ee30f2ef9958a2b624da23a
cselig <cselig@uw.edu>
2016-11-29 16:55:37 -0800

    Update lineSearch.m

--- lineSearch.m
+++ lineSearch.m
@@ -1,44 +1,45 @@
-function [lamda, fcurr] = lineSearch(fn, x0, s)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+function [lamda, i] = lineSearch(fn, x0, s)
+% Search the function fn along the direction s starting from
+% x0. Lamda is the distance that minimizes fn along s, i is number of
+% function evaluations.
 
+% Initialize values
 gr = (1 + sqrt(5)) / 2 - 1;
 f = str2func(fn);
-lbound = findBound(fn, x0, s);
+[lbound, i] = findBound(fn, x0, s);
 la = 0;
 lb = lbound;
 l1 = la + gr * (lb - la);
 l2 = lb - gr * (lb - la);
-i = 0;
-maxItr = 10000;
 errx = 0.0001;
 errf = 0.0001;
 x1 = x0 + l1 .* s;
 x2 = x0 + l2 .* s;
 f1 = f(x1);
 f2 = f(x2);
+i = i + 2;
+maxEval = 10000 + i;
 
-
+% Search using golden section method
 while norm(x1 - x2) > errx && abs(f1 - f2) > errf
     if f1 > f2
         lb = l1;
         l1 = l2;
         l2 = lb - gr * (lb - la);
-    else
+    else % f1 <= f2
         la = l2;
         l2 = l1;
         l1 = la + gr * (lb - la);
-    end
-    i = i + 1;
-    if i > maxItr
-       error('Max number of functional evaluations exceeded') 
     end
     x1 = x0 + l1 .* s;
     x2 = x0 + l2 .* s;
     f1 = f(x1);
     f2 = f(x2);
+    i = i + 2;
+    if i > maxEval
+       error('Max number of function evaluations exceeded') 
+    end
 end
 lamda = la;
-fcurr = f1;
 end
 
